b5507251f7a03f880540faa289b53c4ccfa394e6
maximos-nn <52293321+maximos-nn@users.noreply.github.com>
2019-12-22 21:00:56 +0300

    Б28. Единообразие кавычек в JS

--- source/js/script.js
+++ source/js/script.js
@@ -1,15 +1,15 @@
-var navMain = document.querySelector('.main-nav');
-var navToggle = navMain.querySelector('.main-nav__toggle');
+var navMain = document.querySelector(".main-nav");
+var navToggle = navMain.querySelector(".main-nav__toggle");
 
-navMain.classList.add('main-nav--closed');
+navMain.classList.add("main-nav--closed");
 
-navToggle.addEventListener('click', function(evt) {
+navToggle.addEventListener("click", function(evt) {
   evt.preventDefault();
-  if (navMain.classList.contains('main-nav--closed')) {
-    navMain.classList.remove('main-nav--closed');
-    navMain.classList.add('main-nav--opened');
+  if (navMain.classList.contains("main-nav--closed")) {
+    navMain.classList.remove("main-nav--closed");
+    navMain.classList.add("main-nav--opened");
   } else {
-    navMain.classList.add('main-nav--closed');
-    navMain.classList.remove('main-nav--opened');
+    navMain.classList.add("main-nav--closed");
+    navMain.classList.remove("main-nav--opened");
   }
 });
